a29353651688a8533134d7a869d5048336e9e562
Alexey <fav1290@gmail.com>
2023-09-10 09:08:34 +0300

    Reworked turn functions and minor fixes

---
+++ constants.py
@@ -0,0 +1,8 @@
+PLATFORMS = {
+        'nt' : 'cls',
+        'posix' : 'clear',
+    }
+ICONS = [' X ', ' O ']
+PLAYGROUND_SIZE = 3
+BRAINSTORM_MIN_SEC = 2  #default is 2
+BRAINSTORM_MAX_SEC = 4  #default is 4
